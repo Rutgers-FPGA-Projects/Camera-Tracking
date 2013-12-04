a5af40a928d0be133b41d68e9e3c8c200168e7c1
Haoyang Yu <yhy.haoyang.yu@gmail.com>
2013-12-03 22:37:51 -0500

    This is the version5.0
    After rethinking, I changed the way to express. However theory of
    implementation still the same, for I think it is right.
    I need to test it tomorrow.

--- IR.vhd
+++ IR.vhd
@@ -6,20 +6,22 @@ entity IR is
 	IRDA_RXD: in std_logic;
 	key: in std_logic_vector(0 downto 0);
 	CLOCK_50	:IN STD_LOGIC;
+	ready : out std_logic  ;
 	HEX0, HEX1, HEX2, HEX3, HEX4, HEX5: OUT STD_LOGIC_VECTOR(0 TO 6)
 	);
 end entity;
 
+
 architecture behaviour of IR is
 	component ir_receiver is 
 		port ( 
-			iIRDA: in std_logic;
-			reset: in std_logic;
-			clk_50: in std_logic;
-			--Display the information about CUSTOM CODE bits __HEX7-HEX4
-			--and KEY CODE bits __HEX3-HEX0
-			oData: out std_logic_vector(31 downto 0)
-			);
+				IRDA_RXD: in std_logic;
+				reset: in std_logic;
+				CLOCK_50: in std_logic;
+				--Display the information about CUSTOM CODE bits __HEX7-HEX4
+				--and KEY CODE bits __HEX3-HEX0
+				data_ready : out std_logic ;
+				oData: out std_logic_vector(31 downto 0));
 	end component;
 	
 	component hexDisplay is 
@@ -29,8 +31,9 @@ architecture behaviour of IR is
 	
 	signal display5, display4, display3, display2, display1, display0: std_logic_vector(0 to 6);
 	signal iData: std_logic_vector(31 downto 0); 
+	signal data_readyn : std_logic ;
 begin 
-	I_r: ir_receiver port map(IRDA_RXD,key(0),CLOCK_50,iData);
+	I_r: ir_receiver port map(IRDA_RXD,key(0),CLOCK_50,data_readyn,iData);
 	
 	h0: hexDisplay port map (iData(31 downto 28), display0);
 	h1: hexDisplay port map (iData(27 downto 24), display1);
@@ -46,4 +49,5 @@ begin
 	HEX4<=display4;
 	HEX5<=display5;
 	
+	ready <= data_readyn ;
 end behaviour;
--- ir_receiver.vhd
+++ ir_receiver.vhd
@@ -1,15 +1,17 @@
 LIBRARY ieee; -- Import logic primitives
 USE ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee.std_logic_unsigned.all ;
 
 entity ir_receiver is 
   port ( 
-			iIRDA: in std_logic;
+			IRDA_RXD: in std_logic;
 			reset: in std_logic;
-			clk_50: in std_logic;
+			CLOCK_50: in std_logic;
 			--Display the information about CUSTOM CODE bits __HEX7-HEX4
 			--and KEY CODE bits __HEX3-HEX0
-			oData: out std_logic_vector(31 downto 0)
-			);
+			data_ready : out std_logic ;
+			oData: out std_logic_vector(31 downto 0));
 end entity;
 
 architecture behaviour of ir_receiver is
@@ -35,138 +37,189 @@ architecture behaviour of ir_receiver is
 	signal data_ready_flag: std_logic;
 	signal data: std_logic_vector(31 downto 0);
 	signal data_buffer: std_logic_vector(31 downto 0);
+
 	
 begin 
 --Count the idle_count preparing for state changes from idle to guidance
-	process(clk_50, reset)
+	process(CLOCK_50, reset)
 	begin
 		if (reset='0') then 
 			idle_count<=0;
-		elsif(rising_edge(clk_50) and idle_count_flag='1') then 
-			idle_count<=idle_count+1;
+		elsif rising_edge(CLOCK_50) then 
+			if idle_count_flag='1' then 
+				idle_count<=idle_count+1;
+			else 
+				idle_count<=0;
+			end if;
 		end if;
 	end process;
 	
-	process(clk_50, reset)
-	begin
+	process(CLOCK_50, reset)
+	begin 
 		if (reset='0') then 
 			idle_count_flag<='0';
-		elsif(rising_edge(clk_50) and state=idle and iIRDA='0') then 
-			idle_count_flag<='1';
+		elsif rising_edge(CLOCK_50) then
+			if (state=idle and IRDA_RXD='0') then 
+				idle_count_flag<='1';
+			else 
+				idle_count_flag<='0';
+			end if;
 		end if;
 	end process;
 	
 --Count the guidance_count preparing for state changes from guidance to dataread
-	process(clk_50, reset)
+	process(CLOCK_50, reset)
 	begin
 		if (reset='0') then 
 			guidance_count<=0;
-		elsif(rising_edge(clk_50) and guidance_count_flag='1') then 
-			guidance_count<=guidance_count+1;
+	    elsif rising_edge(CLOCK_50) then
+			if guidance_count_flag='1' then 
+				guidance_count<=guidance_count+1;
+			else 
+				guidance_count<=0;
+			end if;
 		end if;
 	end process;
 	
-	process(clk_50, reset)
+	process(CLOCK_50, reset)
 	begin
 		if (reset='0') then 
 			guidance_count_flag<='0';
-		elsif(rising_edge(clk_50) and state=guidance and iIRDA='1') then 
-			guidance_count_flag<='1';
+		elsif rising_edge(CLOCK_50) then  
+			if (state=guidance and IRDA_RXD='1') then 
+				guidance_count_flag<='1';
+			else 
+				guidance_count_flag<='0';
+			end if;
 		end if;
 	end process;
 	
 --Count the dataread_count preparing for state changes from dataread to idle
-	process(clk_50, reset)
+	process(CLOCK_50, reset)
 	begin
 		if (reset='0') then 
 			dataread_count<=0;
-		elsif(rising_edge(clk_50) and guidance_count_flag='1') then 
-			dataread_count<=dataread_count+1;
+		elsif rising_edge(CLOCK_50) then  
+			if dataread_count_flag='1' then 
+				dataread_count<=dataread_count+1;
+			else 
+				dataread_count<=0;
+			end if;
 		end if;
 	end process;
 	
-	process(clk_50, reset)
-	begin
-		if (reset='0') then 
-			dataread_count_flag<='0';
-		elsif(rising_edge(clk_50) and state=dataread and iIRDA='1') then 
-			dataread_count_flag<='1';
-		end if;
-	end process;
-
---State changes between IDLE, GUIdance, DATaread	
-	process(state, reset, clk_50)
+	process(CLOCK_50, reset)
 	begin 
 		if (reset='0') then 
-			state<=idle;
-		elsif (rising_edge(clk_50)) then
-			case state is
-			when 
-				idle=>
-					if (idle_count>idle2guidanceThreshold) then 
-						state<=guidance;
-					end if;
-			when 
-				guidance=>
-					if (guidance_count>guidance2datareadThreshold) then
-						state<=dataread;
-					end if;
-			when 
-				dataread=>
-					if (dataread_count>data2idleThreshold) then 
-						state<=idle;
-					end if;
-			end case;
+			dataread_count_flag<='0';
+		elsif rising_edge(CLOCK_50) then
+			if (state=dataread and IRDA_RXD='1') then 
+				dataread_count_flag<='1';
+			else 
+				dataread_count_flag<='0';
+			end if;
 		end if;
 	end process;
 	
 --The bitcount to get the info of the bit we have already handled.
-	process(clk_50, reset)
-	begin 
-		if (reset='0') then 
+	process(CLOCK_50, reset)
+	begin
+		if reset='0' then 
 			bitcount<=0;
-		elsif (rising_edge(clk_50) and state= dataread) then 
-			if (dataread_count=20000) then 
+		elsif rising_edge(CLOCK_50) then 
+			if (state=dataread and dataread_count=20000) then 
 				bitcount<=bitcount+1;
+			else
+				bitcount<=0;
 			end if;
 		end if;
 	end process;
 	
+--State changes between IDLE, GUIdance, DATaread	
+	process(state, CLOCK_50)
+	begin 
+		 --First time forget to add reset!
+			if reset='0' then
+				state<=idle;
+			elsif (rising_edge(CLOCK_50)) then
+				case state is
+					when 
+						idle=>
+							if (idle_count>idle2guidanceThreshold) then 
+								state<=guidance;
+							end if;
+					when 
+						guidance=>
+							if (guidance_count>guidance2datareadThreshold) then
+								state<=dataread;
+							end if;
+					when 
+						dataread=>
+							if (dataread_count>=data2idleThreshold or bitcount>=33) then 
+								state<=idle;	
+							end if;
+					when others =>	
+						state <= idle ;
+				end case;
+			end if;
+	end process;
+	
+
 --In the dataread state, to distinguish whether the emitted signal is 0 or 1;
-	process(clk_50, reset)
+--Shift register function;
+	process(CLOCK_50, reset)
 	begin
 		if (reset='0') then 
-			data<= X"00000000";
-		elsif (rising_edge(clk_50) and state=dataread) then 
+			data<= (others=>'0');
+		elsif rising_edge(CLOCK_50) then
 			if (dataread_count>=data_high_duration) then 
 				data(bitcount-1)<='1';
-			else data(bitcount-1)<='0';
+			else 
+				data<=(others=>'0');
 			end if;
 		end if;
 	end process;
 	 				
---Set the data_ready_flag 
-	process(clk_50, reset)
+--Check and Set the data_ready_flag 
+	process(CLOCK_50, reset)
 	begin 
 		if (reset='0') then 
 			data_ready_flag<='0';
-		elsif (rising_edge(clk_50) and bitcount=32) then
-			if (data(31 downto 24)=not data(23 downto 16)) then 
-				data_buffer<=data;
-				data_ready_flag<='1';
-			else data_ready_flag<='0';
+		elsif rising_edge(CLOCK_50) then 
+			if bitcount=32 then
+				if (data(31 downto 24)=(not data(23 downto 16))) then 
+					data_buffer<=data;
+					data_ready_flag<='1';
+				else 
+					data_ready_flag<='0';
+				end if;
+			else 
+				data_ready_flag<='0';
 			end if;
 		end if;
 	end process;
+	
 --Read data
-	process(clk_50, reset)
+	process(CLOCK_50, reset)
 	begin
-		if (reset='0') then 
-			odata<=X"00000000";
-		elsif (rising_edge(clk_50) and data_ready_flag='1') then 
-			odata<=data_buffer;
+		if reset ='0' then 
+			odata<=(others=> '0');
+		elsif rising_edge(CLOCK_50) then 
+			if data_ready_flag='1' then 
+				odata<=data_buffer;
+			--elsif rising_edge(CLOCK_50) then 	
+				--odata(23 downto 0) <= data_buffer(23 downto 0);
+				--if(state = idle) then 
+					--odata(31 downto 24) <= "00010001";
+				--elsif (state = guidance) then
+				--	odata(31 downto 24) <= "00100010"; 
+				--else
+				--	odata(31 downto 24) <= "00110011";
+				--end if;
+			end if;
 		end if;
 	end process;
+data_ready <= data_ready_flag ;
 end behaviour;
 
 
