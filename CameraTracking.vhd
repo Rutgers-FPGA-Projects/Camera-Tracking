96ce6ef532dd27bdb7fae99a26f8eca1fd59ae21
Alexander Hill <ahill6984@gmail.com>
2013-12-06 21:32:49 -0500

    Adding the Camera Code

--- CameraTracking.vhd
+++ CameraTracking.vhd
@@ -18,9 +18,18 @@ entity CameraTracking is
 			EXT_IO: out STD_lOGIC_VECTOR(6 downto 0);  -- this is how the pin mapping labels the external IOs
 			LEDG: out STD_logic_vector(7 downto 0);
 			KEY: in STD_logic_vector(3 downto 0);
-			GPIO: out STD_logic_vector(4 downto 0);
+			--GPIO: out STD_logic_vector(4 downto 0);
 			IRDA_RXD: in std_logic;
-			HEX0, HEX1, HEX2, HEX3, HEX4, HEX5: OUT STD_LOGIC_VECTOR(0 TO 6));
+			HEX0, HEX1, HEX2, HEX3, HEX4, HEX5: OUT STD_LOGIC_VECTOR(0 TO 6);
+			D5M_D: in STD_LOGIC_VECTOR(11 downto 0);
+			D5M_LVAL: in STD_LOGIC;	
+			D5M_FVAL: in STD_LOGIC;
+			D5M_PIXLCLK: in STD_LOGIC;
+			D5M_RESET_N: out STD_LOGIC;
+			D5M_SCLK: out STD_LOGIC;
+			D5M_SDATA: inout STD_LOGIC;
+			D5M_XCLKIN: out STD_LOGIC
+	);
 end;
 
 architecture behavior of CameraTracking is 
@@ -52,8 +61,7 @@ architecture behavior of CameraTracking is
 		);
 	end component;
 	
-	component ir_receiver is 
-		port ( 
+	component ir_receiver is port ( 
 			iIRDA: in std_logic;
 			reset: in std_logic;
 			clk_50: in std_logic;
@@ -64,13 +72,32 @@ architecture behavior of CameraTracking is
 			);
 	end component;
 	
-	component hexDisplay is 
-                port (S: in std_logic_vector(3 downto 0);  -- S is an intermediate signal (NOT A PHYSICAL INPUT)
-                      H: out std_logic_vector(0 to 6));           -- Storage signal for result
+	component hexDisplay is port (
+		S: in std_logic_vector(3 downto 0);  -- S is an intermediate signal (NOT A PHYSICAL INPUT)
+      H: out std_logic_vector(0 to 6));           -- Storage signal for result
 	end component; 
 	
-	component
-		vga_driver port(
+	
+	component CCD_Capture is port(
+					oDATA: out STD_lOGIC_VECTOR(11 downto 0);
+					oDVAL: out STD_LOGIC;
+					oX_Cont: out STD_LOGIC_VECTOR(15 downto 0);
+					oY_Cont: out STD_LOGIC_VECTOR(15 downto 0);
+					oFrame_Cont: out STD_LOGIC_VECTOR(31 downto 0);
+					iDATA: in STD_LOGIC_VECTOR(11 downto 0);
+					iFVAL: in STD_LOGIC;
+					iLVAL: in STD_LOGIC;
+					iSTART: in STD_LOGIC;
+					iEND: in STD_LOGIC;
+					iCLK: in STD_LOGIC;
+					iRST: in STD_LOGIC
+					);
+		end component;
+	
+	
+	
+	
+	component vga_driver is port(
 			VertAddress,HorAddress: out STD_logic_vector(11 downto 0);
 			DataValid: out STD_logic;
 			VGA_CLK: out STD_logic;				-- used by VGA DAC
@@ -101,9 +128,25 @@ architecture behavior of CameraTracking is
 	
 	signal VGAMemReadAddress: STD_logic_vector(16 downto 0);
 	signal VGAMemWriteAddress: STD_logic_vector(16 downto 0);
+	signal VGAMemWriteEnable: STD_LOGIC;
 	signal VGAMemReadData: STD_logic_vector(14 downto 0);
 	signal VGAMemWriteData: STD_logic_vector(14 downto 0);
-
+	
+	signal mup,mdown,mright,mleft: STD_LOGIC;
+	signal horpos,verpos: integer; 
+	
+	
+	-- CCD Camera Signals
+	signal rCCD_DATA: STD_LOGIC_VECTOR(11 downto 0);
+	signal rCCD_LVAL: STD_LOGIC;
+	signal rCCD_FVAL: STD_LOGIC;
+	
+	signal oDATA: STD_lOGIC_VECTOR(11 downto 0);
+	signal oDVAL: STD_LOGIC;
+	signal oX_Cont: STD_LOGIC_VECTOR(15 downto 0);
+	signal oY_Cont: STD_LOGIC_VECTOR(15 downto 0);
+	signal oFrame_Cont: STD_LOGIC_VECTOR(31 downto 0);
+	
 begin
 	
 	-- implement the clock PLL to create a 106 MHz clock
@@ -114,6 +157,15 @@ begin
 		c1	 => clock_106MHz,
 		locked	 => locked
 	);
+	
+	
+	process(D5M_PIXLCLK) begin
+		if(rising_edge(D5M_PIXLCLK)) then
+			rCCD_DATA <= D5M_D;
+			rCCD_FVAL <= D5M_LVAL;
+			rCCD_LVAL <= D5M_LVAL;
+		end if;
+	end process;
 	
 	
 	process (clock_50MHz)
@@ -154,26 +206,90 @@ begin
 		);
 	
 	
-	
+	Camera: CCD_Capture port map(
+					oDATA => oDATA,
+					oDVAL => oDVAL,
+					oX_Cont =>oX_Cont,
+					oY_Cont => oY_Cont,
+					oFrame_Cont => oFrame_Cont,
+					iDATA => rCCD_DATA,
+					iFVAL => rCCD_FVAL,
+					iLVAL => rCCD_LVAL,
+					iSTART => not KEY(3),
+					iEND => not KEY(2),
+					iCLK => not D5M_PIXLCLK,
+					iRST => '1'    -- active low
+					);
+		
+	VGAMemWriteAddress <=  VGA_HorAddress(9 downto 1) & VGA_VertAddress(8 downto 1);
 	
 	TwoPortRam_inst : TwoPortRam PORT MAP (
-		data	 => VGAMemWriteData,
+		data	 => "000" & oDATA, --VGAMemWriteData,
 		rdaddress	 => VGAMemReadAddress,
 		rdclock	 => clock_25MHz,
-		wraddress	 => VGAMemWriteAddress,
-		wrclock	 => clock_25MHz,
-		wren	 => '1',
+		wraddress	 =>  oX_Cont(9 downto 1) & oY_Cont(8 downto 1), --VGAMemWriteAddress,
+		wrclock	 => not D5M_PIXLCLK,
+		wren	 => '1', --VGAMemWriteEnable,
 		q	 => VGAMemReadData 
 	);
 
-	process (VGA_VertAddress,VGA_HorAddress)
-	begin
-		VGAMemWriteAddress <=  VGA_HorAddress(9 downto 1) & VGA_VertAddress(8 downto 1);
-		--std_logic_vector(to_unsigned(unsigned(VGA_HorAddress) + 640 * unsigned(VGA_VertAddress),19)); 
-		VGAMemWriteData <= "00000" & VGA_HorAddress(4 downto 0) & VGA_VertAddress(4 downto 0);
-		
-	end process;
 	
+	mup <= KEY(0);
+	mdown <= KEY(1);
+	mright <= KEY(2);
+	mleft <= KEY(3);
+	
+	
+	process (mup)
+	begin
+		if(rising_edge(mup))then
+			verpos <= verpos + 1;
+		end if;
+	end process;
+
+--	process (mdown)
+--	begin
+--		if(rising_edge(mdown))then
+--			verpos <= verpos - 1;
+--		end if;
+--	end process;
+
+	process (mright)
+	begin
+		if(rising_edge(mright))then
+			horpos <= horpos + 1;
+		end if;
+	end process;
+
+--	process (mleft)
+--	begin
+--		if(rising_edge(mleft))then
+--			horpos <= horpos - 1;
+--		end if;
+--	end process;
+	
+		
+	
+	-- create  a video buffer
+	
+--	process (VGA_VertAddress,VGA_HorAddress)
+--	begin
+--		if(SW(17) = '1')then
+--			VGAMemWriteAddress <=  VGA_HorAddress(9 downto 1) & VGA_VertAddress(8 downto 1);
+--			VGAMemWriteEnable <= '1';
+--			--std_logic_vector(to_unsigned(unsigned(VGA_HorAddress) + 640 * unsigned(VGA_VertAddress),19)); 
+--			
+--			VGAMemWriteData <= "00000" & VGA_HorAddress(9 downto 5) & VGA_VertAddress(8 downto 4);
+--		else
+--			VGAMemWriteAddress <=  std_logic_vector(to_unsigned(horpos,9)) & std_logic_vector(to_unsigned(verpos,8));
+--			VGAMemWriteEnable <= '1';
+--			--std_logic_vector(to_unsigned(unsigned(VGA_HorAddress) + 640 * unsigned(VGA_VertAddress),19)); 
+--			VGAMemWriteData <= "111110000000000";
+--		end if;
+--	end process;
+	
+	
+	-- copy the video memory data to the VGA out lines for the corisponding address
 	process (VGA_VertAddress,VGA_HorAddress)
 	begin
 		VGAMemReadAddress <= VGA_HorAddress(9 downto 1) & VGA_VertAddress(8 downto 1);
@@ -183,40 +299,16 @@ begin
 		VGA_R(2 downto 0) <= "000"; 
 		VGA_G(2 downto 0) <= "000";
 		VGA_B(2 downto 0) <= "000";
-		
-		/*if(VGA_VertAddress > X"0F0") then   -- 0x10
-			VGA_G <= B"11110000";
-			VGA_B <= B"00001111";
-		else
-			VGA_B <= B"11110000";
-			VGA_G <= B"00001111";
-		end if;
-		if(VGA_HorAddress > X"140") then  -- 0x18
-			VGA_R <= B"11000000";
-		else	
-			VGA_R <= B"00000000";
-		end if;
-		*/
 	end process;
 	
 	-- Hook up the IR conections 
 --	I_r: ir_receiver port map(IRDA_RXD,KEY(0),CLOCK_50,IRdata_ready,iData);
-	
-	h0: hexDisplay port map (VGA_VertAddress(3 downto 0), display0);
-	h1: hexDisplay port map (VGA_VertAddress(7 downto 4), display1);
-	h2: hexDisplay port map (VGA_HorAddress(3 downto 0), display2);
-	h3: hexDisplay port map (VGA_HorAddress(7 downto 4), display3);
+	h0: hexDisplay port map (iData(31 downto 28), display0);
+	h1: hexDisplay port map (iData(27 downto 24), display1);
+	h2: hexDisplay port map (iData(23 downto 20), display2);
+	h3: hexDisplay port map (iData(19 downto 16), display3);
 	h4: hexDisplay port map (iData(15 downto 12), display4);
 	h5: hexDisplay port map (iData(11 downto 8), display5);
-	
-
-	
---	h0: hexDisplay port map (iData(31 downto 28), display0);
---	h1: hexDisplay port map (iData(27 downto 24), display1);
---	h2: hexDisplay port map (iData(23 downto 20), display2);
---	h3: hexDisplay port map (iData(19 downto 16), display3);
---	h4: hexDisplay port map (iData(15 downto 12), display4);
---	h5: hexDisplay port map (iData(11 downto 8), display5);
 	
 	HEX0<=display0;
 	HEX1<=display1;
